5c51d7250bea53c54bbcb8aeed0d651f86e2c500
Radostina Todorova <radtod77@icloud.com>
2019-04-25 22:57:44 +0300

    Add files PB CSharp

---
+++ Programming Basics CSharp/Book_Simple-Calculations/Vegetable-Market/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vegetable_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceVegetables = double.Parse(Console.ReadLine());
+            double priceFruits = double.Parse(Console.ReadLine());
+            double kgVegetables = double.Parse(Console.ReadLine());
+            double kgFruits = double.Parse(Console.ReadLine());
+            double amountBGN = kgFruits * priceFruits + kgVegetables * priceVegetables;
+            double amountEUR = amountBGN / 1.94;
+            Console.WriteLine(amountEUR);
+        }
+    }
+}
